209b4c1aaf3f1add2b9c06bf337f46aad027df5b
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-07-28 14:23:57 +0530

    Comparing card choices feature was implemented.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import SingleCard from "./components/SingleCard";
 
@@ -35,6 +35,26 @@ function App() {
   // handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("Those cards match");
+        resetTurn();
+      } else {
+        console.log("Those cards do not match");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
   };
 
   return (
